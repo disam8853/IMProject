201126e48467c3736652d14de084b2a0789bef90
COYADI <sportacushero@yahoo.com>
2020-11-16 20:52:59 +0800

    demo finish

--- DeployModel/code/flask_server.py
+++ DeployModel/code/flask_server.py
@@ -2,6 +2,7 @@ import flask
 from flask import jsonify
 from flask_cors import CORS
 import os
+import json
 
 app = flask.Flask(__name__)
 CORS(app)
@@ -13,9 +14,9 @@ def test():
 
 @app.route('/getAdjMatrix', methods = ['GET'])
 def get_adj_matrix():
-	if os.path.exists('adjacency.txt'):
+	if os.path.exists('adjacency.csv'):
 		adjacency_matrix = []
-		file = open('adjacency.txt')
+		file = open('adjacency.csv')
 		all_rows = file.readlines()
 		for row in all_rows:
 			row = row[:-1].split(',')
@@ -26,5 +27,27 @@ def get_adj_matrix():
 		return jsonify(adjacency_matrix)
 	else:
 		return jsonify({'matrix': []})
+
+@app.route('/getJsonLink/<json_file>', methods = ['GET'])
+def get_json_link(json_file):
+	if os.path.exists('link.csv'):
+		links = []
+		file = open('link.csv')
+		link_list = file.readlines()
+		link_list = link_list[1:]
+		for link in link_list:
+			link = list(map(int, (link[: -1]).split(',')))[1: ]
+			links.append(link)
+		file.close()
+
+		if os.path.exists(f'path/{json_file}'):
+			file = open(f'path/{json_file}')
+			path = file.read()
+			path = json.loads(path)
+			methods = []
+			keys = path.keys()
+			for key in keys:
+				methods.append(path[key])
+			return jsonify({'link': links, 'path': methods})
 
 app.run()
--- DeployModel/code/test_code.py
+++ DeployModel/code/test_code.py
@@ -1,12 +1,22 @@
 import os
+import pandas
+import json
+if os.path.exists('link.csv'):
+	links = []
+	file = open('link.csv')
+	link_list = file.readlines()
+	link_list = link_list[1:]
+	for link in link_list:
+		print(link)
+		link = list(map(int, (link[: -1]).split(',')))[1: ]
+		links.append(link)
+		print(link)
 
-print(os.path.exists('adjacency.txt'))
-file = open('adjacency.txt')
-lines = file.readlines()
-# line = line[:-1].split(',')
-for line in lines:
-	line = line[: -1].split(',')
-	for i in range(len(line)):
-		line[i] = int(line[i])
-	print(line)
-# print(lines)
+# file = open('path/result_20201116_032636.json')
+# r = file.read()
+# r = json.loads(r)
+# methods = []
+# keys = r.keys()
+# for key in keys:
+# 	methods.append({key : r[key]})
+# print(methods)
