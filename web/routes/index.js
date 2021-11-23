3a887a83c0c2ddf62fe4f06986bb1e1bbd250642
disam8853 <wang.sam33@gmail.com>
2021-11-23 14:02:54 +0800

    add new page and decision2

--- web/routes/index.js
+++ web/routes/index.js
@@ -9,6 +9,10 @@ router.get('/', function (req, res, next) {
   res.render('index')
 })
 
+router.get('/admission-control-model', function (req, res, next) {
+  res.render('newModel')
+})
+
 router.get('/flow', (req, res) => {
   res.render('realtime')
 })
